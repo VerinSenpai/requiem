e6bb324e9b31cf28d0d2aa46912368841ab961ab
Kyle Tyo <akikai297@gmail.com>
2021-02-01 17:46:51 -0500

    implemented backend nations tasks and nation command

--- app/plugins/politics_and_war.py
+++ app/plugins/politics_and_war.py
@@ -15,10 +15,13 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+import datetime
 import tortoise
 import aiohttp
 import logging
 import discord
+import asyncio
+import time
 
 from discord.ext import commands, tasks
 from core import models
@@ -30,11 +33,19 @@ _LOGGER = logging.getLogger("requiem.plugins.politics_and_war")
 v2_url = "https://api.politicsandwar.com/graphql"
 
 
+async def update_alliance_history(nation: dict) -> None:
+    await models.AllianceHistory.create(
+        nation_id=nation["id"],
+        alliance_id=nation["alliance_id"],
+        date_recorded=str(datetime.date.today())
+    )
+
+
 async def nation_lookup(target: str or int or discord.User) -> tortoise.queryset.QuerySetSingle or None:
     """
     Looks up a nation in the database using a given target.
     """
-    if isinstance(target, discord.User) or isinstance(target, discord.Member):
+    if isinstance(target, discord.User):
         return await models.Nations.get_or_none(snowflake=target.id)
 
     try:
@@ -57,31 +68,42 @@ async def post(key: str, query: str) -> dict:
             return await response.json(content_type="application/json")
 
 
-class PoliticsAndWarStowingScripts(commands.Cog):
+async def quick_send(ctx: commands.Context, message: str) -> None:
     """
-    Caching scripts for Politics and War.
+    Creates an embed and sends a given message.
+    """
+    embed = discord.Embed(
+        description=message,
+        colour=discord.Colour.purple()
+    )
+    await ctx.send(embed=embed)
+
+
+class Backend(commands.Cog):
+    """
+    Caching and event scripts for Politics and War.
     """
 
     def __init__(self, bot) -> None:
+        self.bot = bot
         self.key = bot.config.pnw_api_key
+        self.create_task = bot.loop.create_task
 
         if not self.key:
             _LOGGER.warning("requiem could not start pnw tasks because a key has not been provided!")
             return
 
-        self.stow_nations.start()
+        self.fetch_nations.start()
 
     def cog_unload(self) -> None:
         """
         Stops all tasks on unload.
         """
-        self.stow_nations.stop()
+        self.fetch_nations.stop()
 
     @tasks.loop(minutes=5)
-    async def stow_nations(self) -> None:
-        """
-        Fetches nation id information and stores it in the database.
-        """
+    async def fetch_nations(self):
+        nations = []
         page = 1
 
         while True:
@@ -90,9 +112,11 @@ class PoliticsAndWarStowingScripts(commands.Cog):
                   nations(first: 500, vmode: false, page:#) {
                     data {
                       id
-                      alliance_id
                       nation_name
                       leader_name
+                      alliance_id
+                      alliance_position
+                      date
                     }
                     paginatorInfo {
                       hasMorePages
@@ -104,21 +128,64 @@ class PoliticsAndWarStowingScripts(commands.Cog):
             data = returned_json.get("data")
 
             if not data:
-                _LOGGER.warning("requiem was unable fetch nations for stowing! is your api key correct?")
+                _LOGGER.warning("requiem was unable fetch nations! is your api key correct?")
                 return
 
-            for nation in data["nations"]["data"]:
-                defaults = {
-                    "alliance_id": nation["alliance_id"],
-                    "nation_name": nation["nation_name"].lower(),
-                    "nation_leader": nation["leader_name"].lower()
-                }
-                await models.Nations.get_or_create(defaults, nation_id=nation["id"])
+            nations.extend(data["nations"]["data"])
 
+            #  blame the api team for this ass
             if not data["nations"]["paginatorInfo"]["hasMorePages"]:
                 break
 
             page += 1
+
+        await self.handle_nations(nations)
+
+    async def handle_nations(self, nations: list) -> None:
+        """
+        Handles stowing of nations in the database and dispatching of nations events.
+        """
+        for nation in nations:
+            defaults = {
+                "nation_name": nation["nation_name"].lower(),
+                "nation_leader": nation["leader_name"].lower(),
+                "alliance_id": nation["alliance_id"],
+                "alliance_position": nation["alliance_position"],
+                "creation_date": nation["date"]
+            }
+            saved, created = await models.Nations.get_or_create(defaults, nation_id=nation["id"])
+            filtered = models.Nations.filter(nation_id=nation["id"])
+
+            if saved.creation_date != nation["date"]:
+                await filtered.update(is_reroll=True)
+                self.create_task(self.dispatch_reroll_notification(nation, saved))
+
+            if int(nation["alliance_id"]) != 0 and nation["alliance_position"] != "APPLICANT":
+                await models.AllianceHistory.get_or_create(
+                    nation_id=nation["id"],
+                    alliance_id=nation["alliance_id"]
+                )
+
+            if saved.alliance_id != int(nation["alliance_id"]):
+                if int(nation["alliance_id"]) != 0 and nation["alliance_position"] == "APPLICANT":
+                    self.create_task(self.dispatch_applicant_notification(nation, saved))
+                await filtered.update(
+                    alliance_id=nation["alliance_id"],
+                    alliance_position=nation["alliance_position"]
+                )
+
+            elif nation["alliance_position"] != saved.alliance_position:
+                await filtered.update(alliance_position=nation["alliance_position"])
+
+    async def dispatch_reroll_notification(self, nation: dict, config: models.Nations) -> None:
+        print(f"Event dispatched ~~ {nation}")
+        shut_up_pylint = self.bot
+        await asyncio.sleep(15)
+
+    async def dispatch_applicant_notification(self, nation: dict, config: models.Nations) -> None:
+        print(f"Event dispatched! ~~ {config.alliance_id} ~~ {nation}")
+        shut_up_pylint = self.bot
+        await asyncio.sleep(15)
 
 
 class PoliticsAndWar(commands.Cog):
@@ -127,6 +194,7 @@ class PoliticsAndWar(commands.Cog):
     """
 
     def __init__(self, bot) -> None:
+        self.bot = bot
         self.key = bot.config.pnw_api_key
 
     async def cog_check(self, ctx) -> bool:
@@ -137,27 +205,21 @@ class PoliticsAndWar(commands.Cog):
 
     @commands.command(brief="Provides information about a given nation.")
     @commands.cooldown(1, 2.5)
-    async def nation(self, ctx: commands.Context, *, target: str) -> None:
+    async def nation(self, ctx: commands.Context, *, target: str = None) -> None:
         """
         Provides information about a given nation.
         """
         if not target:
-            target = ctx.author
+            target = ctx.author.id
 
         try:
-            target = await commands.UserConverter().convert(ctx, target)
+            target = await commands.UserConverter().convert(ctx, str(target))
         except commands.BadArgument:
             pass
 
         entry = await nation_lookup(target)
         if not entry:
-            embed = discord.Embed(
-                description="That nation was not found! If you believe this to be a mistake, wait a few minutes and "
-                            "try again!",
-                colour=discord.Colour.purple()
-            )
-            await ctx.send(embed=embed)
-            return
+            return await quick_send(ctx, "That nation was not found!")
 
         query = """
         {
@@ -192,25 +254,15 @@ class PoliticsAndWar(commands.Cog):
 
         data = returned_json.get("data")
         if not data:
-            embed = discord.Embed(
-                description="The API returned something other than expected! This could be an API key related issue!",
-                colour=discord.Colour.purple()
-            )
-            await ctx.send(embed=embed)
-            return
+            return await quick_send(ctx, "There was an issue with the API response!")
 
         data = data["nations"]["data"]
         if not data:
-            embed = discord.Embed(
-                description="That nation no longer exists!",
-                colour=discord.Colour.purple()
-            )
-            await ctx.send(embed=embed)
-            return
+            return await quick_send(ctx, "That nation no longer exists!")
 
         nation = data[0]
-        leader = f"[{nation['leader_name']}](https://politicsandwar.com/inbox/message/receiver=" \
-                 f"{nation['leader_name'].replace(' ', '%20')})"
+        leader = nation["leader_name"]
+        leader = f"[{leader}](https://politicsandwar.com/inbox/message/receiver={leader.replace(' ', '%20')})"
 
         embed = discord.Embed(
             description=f"[{nation['nation_name']}](https://politicsandwar.com/nation/id={nation['id']}) - {leader}",
@@ -220,11 +272,11 @@ class PoliticsAndWar(commands.Cog):
             embed.add_field(name="Alliance", value=f"[{nation['alliance']['name']}](https://politicsandwar.com/alliance"
                                                    f"/id={nation['alliance_id']})")
             embed.add_field(name="Position", value=nation["alliance_position"].title())
-        embed.add_field(name="Score", value=nation["score"], inline=False)
-        embed.add_field(name="Color", value=nation["color"].title(), inline=False)
-        embed.add_field(name="Cities", value=nation["num_cities"], inline=False)
-        embed.add_field(name="War Policy", value=nation["warpolicy"], inline=False)
-        embed.add_field(name="Domestic Policy", value=nation["dompolicy"], inline=False)
+        embed.add_field(name="Score", value=nation["score"])
+        embed.add_field(name="Color", value=nation["color"].title())
+        embed.add_field(name="Cities", value=nation["num_cities"])
+        embed.add_field(name="War Policy", value=nation["warpolicy"])
+        embed.add_field(name="Domestic Policy", value=nation["dompolicy"])
         embed.add_field(name="Soldiers", value=nation["soldiers"])
         embed.add_field(name="Tanks", value=nation["tanks"])
         embed.add_field(name="Aircraft", value=nation["aircraft"])
@@ -235,5 +287,5 @@ class PoliticsAndWar(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(PoliticsAndWarStowingScripts(bot))
+    bot.add_cog(Backend(bot))
     bot.add_cog(PoliticsAndWar(bot))

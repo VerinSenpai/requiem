6e1935c267f5bafc45f033880877b87cefc5f3fa
Kyle Tyo <akikai297@gmail.com>
2021-02-04 23:54:56 -0500

    removed commands, refined handle_nations and process_nations.

--- app/plugins/politics_and_war.py
+++ app/plugins/politics_and_war.py
@@ -16,11 +16,12 @@
 
 
 import datetime
-import tortoise
 import aiohttp
 import logging
 import discord
 import asyncio
+import string
+import random
 import time
 
 from discord.ext import commands, tasks
@@ -31,52 +32,6 @@ _LOGGER = logging.getLogger("requiem.plugins.politics_and_war")
 
 
 v2_url = "https://api.politicsandwar.com/graphql"
-
-
-async def update_alliance_history(nation: dict) -> None:
-    await models.AllianceHistory.create(
-        nation_id=nation["id"],
-        alliance_id=nation["alliance_id"],
-        date_recorded=str(datetime.date.today())
-    )
-
-
-async def nation_lookup(target: str or int or discord.User) -> tortoise.queryset.QuerySetSingle or None:
-    """
-    Looks up a nation in the database using a given target.
-    """
-    if isinstance(target, discord.User):
-        return await models.Nations.get_or_none(snowflake=target.id)
-
-    try:
-        return await models.Nations.get_or_none(nation_id=int(target))
-
-    except ValueError:
-        by_name = await models.Nations.get_or_none(nation_name=target.lower())
-        by_leader = await models.Nations.get_or_none(nation_leader=target.lower())
-        return by_name or by_leader
-
-
-async def post(key: str, query: str) -> dict:
-    """
-    Posts to the pnw api with a key and query.
-    """
-    payload = {"api_key": key, "query": query}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(v2_url, json=payload) as response:
-            return await response.json(content_type="application/json")
-
-
-async def quick_send(ctx: commands.Context, message: str) -> None:
-    """
-    Creates an embed and sends a given message.
-    """
-    embed = discord.Embed(
-        description=message,
-        colour=discord.Colour.purple()
-    )
-    await ctx.send(embed=embed)
 
 
 class Backend(commands.Cog):
@@ -90,103 +45,145 @@ class Backend(commands.Cog):
         self.create_task = bot.loop.create_task
 
         if not self.key:
-            _LOGGER.warning("requiem could not start pnw tasks because a key has not been provided!")
+            _LOGGER.warning("requiem could not start pnw backend tasks because a key has not been provided!")
             return
 
-        self.fetch_nations.start()
+        self.handle_nations.error(self.on_task_error)
+        self.handle_nations.start()
 
     def cog_unload(self) -> None:
         """
         Stops all tasks on unload.
         """
-        self.fetch_nations.stop()
+        self.handle_nations.stop()
+
+    async def on_task_error(self, *args, **kwargs) -> None:
+        """
+        Handles catching TypeErrors raised while fetching API queries.
+        """
+        exc = args[1]
+
+        if isinstance(exc, TypeError):
+            _LOGGER.warning("requiem received an error from the API!")
+            return
+
+        await self.bot.report_error(exc, "executing a task in politics_and_war.Backend")
 
     @tasks.loop(minutes=5)
-    async def fetch_nations(self):
+    async def handle_nations(self) -> None:
+        """
+        Handles fetching of nations. When completed, starts process_nations.
+        """
         nations = []
         page = 1
 
         while True:
             query = """
                 {
-                  nations(first: 500, vmode: false, page:#) {
+                  nations(first: 500, page:#) {
                     data {
                       id
-                      nation_name
-                      leader_name
                       alliance_id
                       alliance_position
+                      nation_name
+                      color
+                      num_cities
+                      vmode
+                      beigeturns
                       date
-                    }
-                    paginatorInfo {
-                      hasMorePages
                     }
                   }
                 }
                 """.replace("#", str(page))
-            returned_json = await post(self.key, query)
-            data = returned_json.get("data")
+            payload = {"api_key": self.key, "query": query}
 
-            if not data:
-                _LOGGER.warning("requiem was unable fetch nations! is your api key correct?")
-                return
+            async with aiohttp.ClientSession() as session:
+                async with session.get(v2_url, json=payload) as response:
+                    returned_json = await response.json(content_type="application/json")
 
-            nations.extend(data["nations"]["data"])
+            fetched_nations = returned_json["data"]["nations"]["data"]
 
-            #  blame the api team for this ass
-            if not data["nations"]["paginatorInfo"]["hasMorePages"]:
+            if not fetched_nations:
                 break
+
+            nations.extend(fetched_nations)
 
             page += 1
 
-        await self.handle_nations(nations)
-
-    async def handle_nations(self, nations: list) -> None:
+    async def process_nations(self, nations: list) -> None:
         """
-        Handles stowing of nations in the database and dispatching of nations events.
+        Handles stowing and processing of nation events.
         """
         for nation in nations:
             defaults = {
-                "nation_name": nation["nation_name"].lower(),
-                "nation_leader": nation["leader_name"].lower(),
+                "nation_name": nation["nation_name"],
                 "alliance_id": nation["alliance_id"],
                 "alliance_position": nation["alliance_position"],
+                "color": nation["color"],
+                "cities": nation["num_cities"],
+                "vmode_turns": nation["vmode"],
+                "beige_turns": nation["beigeturns"],
                 "original_creation_date": nation["date"],
                 "latest_creation_date": nation["date"]
             }
-            saved, created = await models.Nations.get_or_create(defaults, nation_id=nation["id"])
-            filtered = models.Nations.filter(nation_id=nation["id"])
+            entry, created = await models.NationIndex.get_or_create(defaults=defaults, nation_id=nation["id"])
 
-            if saved.latest_creation_date != nation["date"]:
-                await filtered.update(latest_creation_date=nation["date"])
-                self.create_task(self.dispatch_reroll_notification(nation, saved))
+            if created:
+                continue
 
-            if int(nation["alliance_id"]) != 0 and nation["alliance_position"] != "APPLICANT":
-                await models.AllianceHistory.get_or_create(
-                    nation_id=nation["id"],
-                    alliance_id=nation["alliance_id"]
-                )
+            if nation["alliance_id"] != entry.alliance_id:
+                if nation["alliance_position"] == "APPLICANT":
+                    await self.report_applicant(nation, entry)
+                entry.alliance_id = nation["alliance_id"]
 
-            if saved.alliance_id != int(nation["alliance_id"]):
-                if int(nation["alliance_id"]) != 0 and nation["alliance_position"] == "APPLICANT":
-                    self.create_task(self.dispatch_applicant_notification(nation, saved))
-                await filtered.update(
-                    alliance_id=nation["alliance_id"],
-                    alliance_position=nation["alliance_position"]
-                )
+            if nation["alliance_position"] != entry.alliance_position:
+                entry.alliance_position = nation["alliance_position"]
 
-            elif nation["alliance_position"] != saved.alliance_position:
-                await filtered.update(alliance_position=nation["alliance_position"])
+            if nation["date"] != entry.latest_creation_date:
+                await self.report_reroll(nation, entry)
+                entry.latest_creation_date = nation["date"]
 
-    async def dispatch_reroll_notification(self, nation: dict, config: models.Nations) -> None:
-        print(f"Event dispatched ~~ {nation}")
+            if nation["vmode"] > entry.vmode_turns == 0:
+                await self.report_enter_vmode(nation, entry)
+                entry.vmode_turns = nation["vmode"]
+
+            elif entry.vmode_turns > nation["vmode"] == 0:
+                await self.report_exit_vmode(nation, entry)
+                entry.vmode_turns = nation["vmode"]
+
+            if nation["beigeturns"] > entry.beige_turns == 0:
+                await self.report_enter_beige(nation, entry)
+                entry.beige_turns = nation["beigeturns"]
+
+            elif entry.beige_turns > nation["beigeturns"] == 0:
+                await self.report_exit_beige(nation, entry)
+                entry.beige_turns = nation["beigeturns"]
+
+            await entry.save()
+
+    async def report_applicant(self, nation, entry) -> None:
         shut_up_pylint = self.bot
-        await asyncio.sleep(15)
+        print(f"NATION APPLIED TO JOIN ALLIANCE {nation}")
 
-    async def dispatch_applicant_notification(self, nation: dict, config: models.Nations) -> None:
-        print(f"Event dispatched! ~~ {config.alliance_id} ~~ {nation}")
+    async def report_reroll(self, nation, entry) -> None:
         shut_up_pylint = self.bot
-        await asyncio.sleep(15)
+        print(f"NATION REROLLED {nation}")
+
+    async def report_enter_vmode(self, nation, entry) -> None:
+        shut_up_pylint = self.bot
+        print(f"NATION ENTERED VMODE {nation}")
+
+    async def report_exit_vmode(self, nation, entry) -> None:
+        shut_up_pylint = self.bot
+        print(f"NATION EXITED VMODE {nation}")
+
+    async def report_enter_beige(self, nation, entry) -> None:
+        shut_up_pylint = self.bot
+        print(f"NATION EXITED BEIGE {nation}")
+
+    async def report_exit_beige(self, nation, entry) -> None:
+        shut_up_pylint = self.bot
+        print(f"NATION EXITED BEIGE {nation}")
 
 
 class PoliticsAndWar(commands.Cog):
@@ -203,97 +200,6 @@ class PoliticsAndWar(commands.Cog):
         Returns true if an api key has been configured.
         """
         return bool(self.key)
-
-    @commands.command(brief="Provides information about a given nation.")
-    @commands.cooldown(1, 2.5)
-    async def nation(self, ctx: commands.Context, *, target: str = None) -> None:
-        """
-        Provides information about a given nation.
-        """
-        if not target:
-            target = ctx.author.id
-
-        try:
-            target = await commands.UserConverter().convert(ctx, str(target))
-        except commands.BadArgument:
-            pass
-
-        entry = await nation_lookup(target)
-        if not entry:
-            return await quick_send(ctx, "That nation was not found!")
-
-        query = """
-        {
-          nations(first: 1, id: #) {
-            data {
-              id
-              alliance {
-                name
-              }
-              alliance_id
-              alliance_position
-              nation_name
-              leader_name
-              date
-              warpolicy
-              dompolicy
-              color
-              num_cities
-              score
-              last_active
-              soldiers
-              tanks
-              aircraft
-              ships
-              missiles
-              nukes
-              projects
-            }
-          }
-        }
-        """.replace("#", str(entry.nation_id))
-        returned_json = await post(self.key, query)
-
-        data = returned_json.get("data")
-        if not data:
-            return await quick_send(ctx, "There was an issue with the API response!")
-
-        data = data["nations"]["data"]
-        if not data:
-            return await quick_send(ctx, "That nation no longer exists!")
-
-        nation = data[0]
-        leader = nation["leader_name"]
-        leader = f"[{leader}](https://politicsandwar.com/inbox/message/receiver={leader.replace(' ', '%20')})"
-
-        embed = discord.Embed(
-            description=f"[{nation['nation_name']}](https://politicsandwar.com/nation/id={nation['id']}) - {leader}",
-            colour=discord.Colour.purple()
-        )
-
-        if nation["date"] != entry.original_creation_date:
-            embed.add_field(name="Re-roll Alert", value="This nation is a re-roll!", inline=False)
-            embed.add_field(name="Original Creation Date", value=entry.original_creation_date, inline=False)
-            embed.add_field(name="Latest Creation Date", value=nation["date"], inline=False)
-
-        if nation["alliance"]:
-            embed.add_field(name="Alliance", value=f"[{nation['alliance']['name']}](https://politicsandwar.com/alliance"
-                                                   f"/id={nation['alliance_id']})")
-            embed.add_field(name="Position", value=nation["alliance_position"].title())
-
-        embed.add_field(name="Score", value=nation["score"], inline=False)
-        embed.add_field(name="Cities", value=nation["num_cities"], inline=False)
-        embed.add_field(name="War Policy", value=nation["warpolicy"])
-        embed.add_field(name="Domestic Policy", value=nation["dompolicy"])
-        embed.add_field(name="Color", value=nation["color"].title(), inline=False)
-
-        embed.add_field(name="Soldiers", value=nation["soldiers"])
-        embed.add_field(name="Tanks", value=nation["tanks"])
-        embed.add_field(name="Aircraft", value=nation["aircraft"])
-        embed.add_field(name="Ships", value=nation["ships"])
-        embed.add_field(name="Missiles", value=nation["missiles"])
-        embed.add_field(name="Nukes", value=nation["nukes"])
-        await ctx.send(embed=embed)
 
 
 def setup(bot):

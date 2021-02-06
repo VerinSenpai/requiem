00995ddd3db76490c421e664a6450ee82f036b6c
Kyle Tyo <akikai297@gmail.com>
2021-02-06 00:37:30 -0500

    reintegrated nation command, tweaked backend script to include saving leader_name, added color_change event

--- app/plugins/politics_and_war.py
+++ app/plugins/politics_and_war.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+import contextlib
 import datetime
 import aiohttp
 import logging
@@ -32,6 +33,30 @@ _LOGGER = logging.getLogger("requiem.plugins.politics_and_war")
 
 
 v2_url = "https://api.politicsandwar.com/graphql"
+
+
+async def nation_lookup(ctx: commands.Context, target: str) -> models.NationIndex:
+    target = str(target).lower()
+
+    with contextlib.suppress(commands.BadArgument):
+        user = await commands.UserConverter().convert(ctx, target)
+        return await models.NationIndex.get_or_none(snowflake=user.id)
+
+    if target.isnumeric():
+        return await models.NationIndex.get_or_none(nation_id=int(target))
+
+    by_name = await models.NationIndex.get_or_none(nation_name=target)
+    by_leader = await models.NationIndex.get_or_none(leader_name=target)
+
+    return by_leader or by_name
+
+
+async def quick_send_embed(ctx, message) -> None:
+    """
+    Creates an embed with a given message and sends it.
+    """
+    embed = discord.Embed(description=message, colour=discord.Colour.purple())
+    await ctx.send(embed=embed)
 
 
 class Backend(commands.Cog):
@@ -86,6 +111,7 @@ class Backend(commands.Cog):
                       alliance_id
                       alliance_position
                       nation_name
+                      leader_name
                       color
                       num_cities
                       vmode
@@ -118,7 +144,8 @@ class Backend(commands.Cog):
         """
         for nation in nations:
             defaults = {
-                "nation_name": nation["nation_name"],
+                "nation_name": nation["nation_name"].lower(),
+                "leader_name": nation["leader_name"].lower(),
                 "alliance_id": nation["alliance_id"],
                 "alliance_position": nation["alliance_position"],
                 "color": nation["color"],
@@ -132,6 +159,17 @@ class Backend(commands.Cog):
 
             if created:
                 continue
+
+            if nation["nation_name"].lower() != entry.nation_name:
+                entry.nation_name = nation["nation_name"].lower()
+
+            if nation["leader_name"].lower() != entry.leader_name:
+                entry.leader_name = nation["leader_name"].lower()
+
+            if nation["color"] != entry.color:
+                if nation["color"] != "beige":
+                    self.create_task(self.report_color_change(nation, entry))
+                entry.color = nation["color"]
 
             if int(nation["alliance_id"]) != entry.alliance_id:
                 if nation["alliance_position"] == "APPLICANT":
@@ -149,7 +187,7 @@ class Backend(commands.Cog):
                 self.create_task(self.report_enter_vmode(nation, entry))
                 entry.vmode_turns = nation["vmode"]
 
-            elif entry.vmode_turns > int(nation["vmode"]) == 0:
+            if entry.vmode_turns > int(nation["vmode"]) == 0:
                 self.create_task(self.report_exit_vmode(nation, entry))
                 entry.vmode_turns = nation["vmode"]
 
@@ -157,7 +195,7 @@ class Backend(commands.Cog):
                 self.create_task(self.report_enter_beige(nation, entry))
                 entry.beige_turns = nation["beigeturns"]
 
-            elif entry.beige_turns > int(nation["beigeturns"]) == 0:
+            if entry.beige_turns > int(nation["beigeturns"]) == 0:
                 self.create_task(self.report_exit_beige(nation, entry))
                 entry.beige_turns = nation["beigeturns"]
 
@@ -193,6 +231,11 @@ class Backend(commands.Cog):
         print(f"NATION EXITED BEIGE {nation}")
         await asyncio.sleep(15)
 
+    async def report_color_change(self, nation, entry) -> None:
+        shut_up_pylint = self.bot
+        print(f"NATION CHANGED COLOR {nation}")
+        await asyncio.sleep(15)
+
 
 class PoliticsAndWar(commands.Cog):
     """
@@ -208,6 +251,103 @@ class PoliticsAndWar(commands.Cog):
         Returns true if an api key has been configured.
         """
         return bool(self.key)
+
+    @commands.command(aliases=["pwnat"], brief="View a given nation.")
+    @commands.cooldown(1, 2.5)
+    async def pwnation(self, ctx: commands.Context, *, target: str = None) -> None:
+        """
+        View information about a nation.
+
+        You can look a nation up by its id, name, or a discord user (if they've linked themselves to their nation)
+        """
+        if not target:
+            target = ctx.author.id
+
+        entry = await nation_lookup(ctx, target)
+
+        if not entry:
+            message = "I was unable to find a nation matching your query!"
+            return await quick_send_embed(ctx, message)
+
+        query = """
+            {
+              nations(first: 1, id: #) {
+                data {
+                  id
+                  alliance {
+                    name
+                  }
+                  alliance_id
+                  alliance_position
+                  nation_name
+                  leader_name
+                  continent
+                  warpolicy
+                  dompolicy
+                  color
+                  num_cities
+                  score
+                  vmode
+                  beigeturns
+                  date
+                  soldiers
+                  tanks
+                  aircraft
+                  ships
+                  missiles
+                  nukes
+                }
+              }
+            }
+            """.replace("#", str(entry.nation_id))
+        payload = {"api_key": self.key, "query": query}
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(v2_url, json=payload) as response:
+                returned_json = await response.json(content_type="application/json")
+
+        if not isinstance(returned_json, dict):
+            message = "The API returned an unexpected response!"
+            return await quick_send_embed(ctx, message)
+
+        fetched_nations = returned_json["data"]["nations"]["data"]
+
+        if not fetched_nations:
+            message = "The nation specified no longer exists!"
+            return await quick_send_embed(ctx, message)
+
+        nation = fetched_nations[0]
+        leader = nation["leader_name"]
+
+        nation_str = f"[{nation['nation_name']}](https://politicsandwar.com/nation/id={nation['id']})"
+        leader_str = f"[{leader}](https://politicsandwar.com/inbox/message/receiver={leader.replace(' ', '%20')})"
+
+        embed = discord.Embed(description=f"{nation_str} - {leader_str}", colour=discord.Colour.purple())
+
+        if entry.latest_creation_date != entry.original_creation_date:
+            embed.add_field(name="Notice", value=f"This nation is a re-roll.", inline=False)
+            embed.add_field(name="Original Creation Date", value=entry.original_creation_date, inline=False)
+            embed.add_field(name="Latest Creation Date", value=nation["date"], inline=False)
+
+        if nation["alliance"]:
+            alliance_name = nation["alliance"]["name"].title()
+            alliance_str = f"[{alliance_name}](https://politicsandwar.com/alliance/id={nation['alliance_id']})"
+            embed.add_field(name="Alliance", value=alliance_str)
+            embed.add_field(name="Position", value=nation["alliance_position"].title())
+
+        embed.add_field(name="Score", value=nation["score"], inline=False)
+        embed.add_field(name="Color", value=nation["color"].title(), inline=False)
+        embed.add_field(name="Cities", value=nation["num_cities"], inline=False)
+        embed.add_field(name="War Policy", value=nation["warpolicy"], inline=False)
+        embed.add_field(name="Domestic Policy", value=nation["dompolicy"], inline=False)
+        embed.add_field(name="Soldiers", value=nation["soldiers"])
+        embed.add_field(name="Tanks", value=nation["tanks"])
+        embed.add_field(name="Aircraft", value=nation["aircraft"])
+        embed.add_field(name="Ships", value=nation["ships"])
+        embed.add_field(name="Missiles", value=nation["missiles"])
+        embed.add_field(name="Nukes", value=nation["nukes"])
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):

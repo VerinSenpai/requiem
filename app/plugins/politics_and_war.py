cd7539650f93b595e1e51b4884616391327a2794
Kyle Tyo <akikai297@gmail.com>
2021-04-06 21:26:58 -0400

    Revert "simplified alot"
    This reverts commit 212222e4faadd8de1a51e39225f04d05e2c7c608.

--- app/plugins/politics_and_war.py
+++ app/plugins/politics_and_war.py
@@ -19,7 +19,6 @@ import logging
 import discord
 import typing
 import aiohttp
-import time
 
 
 from datetime import datetime, timezone, timedelta
@@ -90,8 +89,9 @@ class Backend(commands.Cog):
             )
             return
 
-        self.fetch_nations.start()
-        self.fetch_alliances.start()
+        self.handle_nations.start()
+        self.handle_alliances.start()
+        self.handle_wars.start()
 
     def cog_unload(self) -> None:
         """
@@ -100,14 +100,13 @@ class Backend(commands.Cog):
         self.handle_nations.stop()
 
     @tasks.loop(minutes=5)
-    async def fetch_nations(self) -> None:
+    async def handle_nations(self) -> None:
         """
         Handles fetching of nations. When completed, starts process_nations.
         """
         nations = []
         page = 1
 
-        start = time.time()
         while True:
             query = """
             {
@@ -118,10 +117,21 @@ class Backend(commands.Cog):
                   alliance_position
                   nation_name
                   leader_name
+                  warpolicy
+                  dompolicy
+                  color
+                  num_cities
+                  score
                   vmode
                   beigeturns
                   last_active
                   date
+                  soldiers
+                  tanks
+                  aircraft
+                  ships
+                  missiles
+                  nukes
                 }
               }
             }
@@ -142,7 +152,7 @@ class Backend(commands.Cog):
 
     async def process_nations(self, nations: list) -> None:
         """
-        Handles stowing and processing of nations.
+        Handles stowing and processing of nation events.
         """
         for nation in nations:
             defaults = {
@@ -150,10 +160,21 @@ class Backend(commands.Cog):
                 "leader": nation["leader_name"].lower(),
                 "alliance": nation["alliance_id"],
                 "alliance_position": nation["alliance_position"],
+                "war_policy": nation["warpolicy"],
+                "dom_policy": nation["dompolicy"],
+                "color": nation["color"],
+                "cities": nation["num_cities"],
+                "score": nation["score"],
                 "vmode_turns": nation["vmode"],
                 "beige_turns": nation["beigeturns"],
                 "original_creation_date": nation["date"],
                 "latest_creation_date": nation["date"],
+                "soldiers": nation["soldiers"],
+                "tanks": nation["tanks"],
+                "aircraft": nation["aircraft"],
+                "ships": nation["ships"],
+                "missiles": nation["missiles"],
+                "nukes": nation["nukes"],
             }
             entry, created = await models.Nations.get_or_create(
                 defaults=defaults, id=nation["id"]
@@ -191,14 +212,41 @@ class Backend(commands.Cog):
             entry.leader = nation["leader_name"].lower()
             entry.alliance = nation["alliance_id"]
             entry.alliance_position = nation["alliance_position"]
+            entry.latest_creation_date = nation["date"]
             entry.vmode_turns = nation["vmode"]
             entry.beige_turns = nation["beigeturns"]
-            entry.latest_creation_date = nation["date"]
+            entry.cities = nation["num_cities"]
+            entry.color = nation["color"]
+            entry.color = nation["color"]
 
-            await entry.save()        
+            await entry.save()
 
-    @tasks.loop(minutes=10)
-    async def fetch_alliances(self) -> None:
+    async def report_applicant(self, nation: dict, entry: models.Nations) -> None:
+        channel = self.bot.get_channel(812669787511324692)
+        await channel.send(f"NATION APPLIED TO JOIN ALLIANCE {nation}")
+
+    async def report_reroll(self, nation: dict, entry: models.Nations) -> None:
+        channel = self.bot.get_channel(812669787511324692)
+        await channel.send(f"NATION REROLLED {nation}")
+
+    async def report_enter_vmode(self, nation: dict, entry: models.Nations) -> None:
+        channel = self.bot.get_channel(812669787511324692)
+        await channel.send(f"NATION ENTERED VMODE {nation}")
+
+    async def report_exit_vmode(self, nation: dict, entry: models.Nations) -> None:
+        channel = self.bot.get_channel(812669787511324692)
+        await channel.send(f"NATION EXITED VMODE {nation}")
+
+    async def report_enter_beige(self, nation: dict, entry: models.Nations) -> None:
+        channel = self.bot.get_channel(812669787511324692)
+        await channel.send(f"NATION ENTERED BEIGE {nation}")
+
+    async def report_exit_beige(self, nation: dict, entry: models.Nations) -> None:
+        channel = self.bot.get_channel(812669787511324692)
+        await channel.send(f"NATION EXITED BEIGE {nation}")
+
+    @tasks.loop(minutes=5)
+    async def handle_alliances(self) -> None:
         """
         Handles fetching of alliances.
         """
@@ -273,29 +321,36 @@ class Backend(commands.Cog):
 
             await entry.save()
 
-    async def report_applicant(self, nation: dict, entry: models.Nations) -> None:
-        channel = self.bot.get_channel(812669787511324692)
-        await channel.send(f"NATION APPLIED TO JOIN ALLIANCE {nation}")
+    @tasks.loop(minutes=5)
+    async def handle_wars(self) -> None:
+        """
+        Handle fetching of wars.
+        """
+        query = """
+        {
+          wars {
+            id
+            date
+            reason
+            war_type
+            attid
+            defid
+          }
+        }
+        """
+        response = await v3_post(self.key, query)
+        wars = response["data"]["wars"]["data"]
 
-    async def report_reroll(self, nation: dict, entry: models.Nations) -> None:
-        channel = self.bot.get_channel(812669787511324692)
-        await channel.send(f"NATION REROLLED {nation}")
+        await self.process_wars(wars)
 
-    async def report_enter_vmode(self, nation: dict, entry: models.Nations) -> None:
-        channel = self.bot.get_channel(812669787511324692)
-        await channel.send(f"NATION ENTERED VMODE {nation}")
+    async def process_wars(self, wars: list):
+        dispatched = []
 
-    async def report_exit_vmode(self, nation: dict, entry: models.Nations) -> None:
-        channel = self.bot.get_channel(812669787511324692)
-        await channel.send(f"NATION EXITED VMODE {nation}")
+        for war in wars:
+            if war["id"] in dispatched:
+                continue
 
-    async def report_enter_beige(self, nation: dict, entry: models.Nations) -> None:
-        channel = self.bot.get_channel(812669787511324692)
-        await channel.send(f"NATION ENTERED BEIGE {nation}")
-
-    async def report_exit_beige(self, nation: dict, entry: models.Nations) -> None:
-        channel = self.bot.get_channel(812669787511324692)
-        await channel.send(f"NATION EXITED BEIGE {nation}")
+            self.create_task()
 
 
 class PoliticsAndWar(commands.Cog, name="politics and war"):
@@ -309,6 +364,133 @@ class PoliticsAndWar(commands.Cog, name="politics and war"):
         """
         if ctx.bot.config.pnw_api_key and ctx.bot.uptime[0] > 5:
             return True
+
+    @commands.command(aliases=["pwla"], brief="List all currently active alliances.")
+    @commands.cooldown(1, 2.5)
+    async def pwlistaa(self, ctx: commands.Context) -> None:
+        """
+        List all currently active alliances.
+        """
+        entries = await models.Alliances.all()
+        active = [aa for aa in entries if (datetime.now(timezone.utc) - aa.last_updated).days == 0]
+
+        pages = []
+        rank = 1
+        embed = discord.Embed(colour=discord.Colour.purple())
+
+        for alliance in sorted(active, key=lambda aa: aa.score, reverse=True):
+            embed.add_field(
+                name=f"{rank} of {len(active)}",
+                value=f"[{alliance.name.title()} ({alliance.acronym})]({base_url}/alliance/id={alliance.id})",
+                inline=False
+            )
+            rank += 1
+
+            if len(embed.fields) == 10:
+                pages.append(embed)
+                embed = discord.Embed(colour=discord.Colour.purple())
+
+        if embed not in pages:
+            pages.append(embed)
+
+        await paginator.Paginator(pages).start(ctx)
+
+    @commands.group(aliases=["pwna"], brief="View a given nation.")
+    @commands.cooldown(1, 2.5)
+    async def pwnation(
+        self, ctx: commands.Context, *, target: typing.Union[discord.User, str] = None
+    ) -> None:
+        """
+        View information about a nation.
+
+        You can look a nation up by its id, name, or a discord user (if they've linked themselves to their nation)
+        If you don't specify a target, a lookup is performed for a nation linked to your discord.
+        """
+        target = target or ctx.author.id
+
+        if isinstance(target, discord.User):
+            target = target.id
+
+        entry = await nation_lookup(target)
+
+        if entry:
+            since_update = datetime.now(timezone.utc) - entry.last_updated
+
+            name = (
+                f"{entry.name} (formerly {entry.prev_name})"
+                if entry.prev_name
+                else entry.name
+            )
+            leader = (
+                f"{entry.leader} (formerly {entry.leader})"
+                if entry.prev_leader
+                else entry.leader
+            )
+
+            name_url = f"[{name.title()}]({base_url}/nation/id={entry.id})"
+            leader_url = f"[{leader.title()}]({base_url}/inbox/message/receiver={leader.replace(' ', '+')})"
+
+            embed = discord.Embed(
+                description=f"{name_url} - {leader_url}", colour=discord.Colour.purple()
+            )
+
+            if since_update.days > 0:
+                embed.add_field(
+                    name="Deleted Nation",
+                    value="You are viewing a nation that no longer exists.",
+                    inline=False,
+                )
+
+            if entry.original_creation_date != entry.latest_creation_date:
+                embed.add_field(
+                    name="Re-roll",
+                    value="You are viewing a nation that has re-rolled.",
+                    inline=False,
+                )
+
+            if entry.vmode_turns:
+                embed.add_field(
+                    name="Vacation Mode",
+                    value=f"This nation is in vacation mode for {entry.vmode_turns} more turns.",
+                    inline=False,
+                )
+
+            if entry.beige_turns:
+                embed.add_field(
+                    name="Beige",
+                    value=f"This nation is on beige for {entry.beige_turns} more turns.",
+                    inline=False,
+                )
+
+            if alliance := await alliance_lookup(entry.alliance):  # this probably angers some of you. that's okay.
+                embed.add_field(name="Alliance", value=f"{alliance.name.title()} ({alliance.acronym})")
+                embed.add_field(name="Position", value=f"{entry.alliance_position.title()}")
+                embed.add_field(name="IRC/Discord", value=alliance.irc, inline=False)
+
+            elif entry.alliance:
+                embed.add_field(name="Alliance", value="Unknown Alliance", inline=False)
+
+            embed.add_field(name="War Policy", value=entry.war_policy.title())
+            embed.add_field(name="Domestic Policy", value=entry.dom_policy.title())
+            embed.add_field(name="Color", value=entry.color.title())
+            embed.add_field(name="Cities", value=str(entry.cities))
+            embed.add_field(name="Score", value=f"{entry.score:,}")
+            embed.add_field(name="Soldiers", value=f"{entry.soldiers:,}")
+            embed.add_field(name="Tanks", value=f"{entry.tanks:,}")
+            embed.add_field(name="Aircraft", value=f"{entry.aircraft:,}")
+            embed.add_field(name="Ships", value=f"{entry.ships:,}")
+            embed.add_field(name="Missiles", value=f"{entry.missiles:,}")
+            embed.add_field(name="Nukes", value=f"{entry.nukes:,}")
+            embed.set_footer(text=f"Nation Last Updated {entry.last_updated}")
+
+        else:
+            embed = discord.Embed(
+                description="That nation does not exist.",
+                colour=discord.Colour.purple(),
+            )
+
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Backend(bot))

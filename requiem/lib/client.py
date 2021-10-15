acea2aed7a57e334d6e8c69c629e077ea3b2e8d3
Kyle Tyo <akikai297@gmail.com>
2021-10-15 16:04:59 -0400

    added setup database, renamed some events

--- requiem/lib/client.py
+++ requiem/lib/client.py
@@ -19,9 +19,13 @@ from lightbulb import slash_commands
 from lib.models import Credentials
 
 import lightbulb
+import tortoise
 import logging
+import asyncpg
+import socket
 import typing
 import hikari
+import yarl
 import abc
 import os
 
@@ -42,8 +46,12 @@ class Requiem(lightbulb.Bot, abc.ABC):
             default_enabled_guilds=credentials.enabled_guilds,
         )
 
-        self.subscribe(hikari.StartingEvent, self.on_starting_event)
-        self.subscribe(hikari.StoppingEvent, self.on_stopping_event)
+        self.credentials = credentials
+
+        self.subscribe(hikari.StartingEvent, self.setup_database)
+        self.subscribe(hikari.StartingEvent, self.load_extensions_on_start)
+        self.subscribe(hikari.StoppingEvent, self.clean_up_on_closing)
+        self.subscribe(lightbulb.SlashCommandCompletionEvent, self.log_successful_command_invoke)
 
     @property
     def all_extensions(self) -> typing.Generator[str, any, None]:
@@ -56,11 +64,43 @@ class Requiem(lightbulb.Bot, abc.ABC):
             if plugin not in ("__init__.py", "__pycache__")
         )
 
-    async def on_starting_event(self, event: hikari.StartingEvent) -> None:
+    async def setup_database(self, event: hikari.StartingEvent) -> None:
+        """
+        Attempts a connection to a postgresql server. Falls back to using sqlite.
+        """
+        url = yarl.URL.build(
+            scheme="postgres",
+            host=self.credentials.postgres_host,
+            port=self.credentials.postgres_port,
+            user=self.credentials.postgres_user,
+            password=self.credentials.postgres_password,
+            path=f"/{self.credentials.postgres_database}",
+        )
+        modules = {"models": ["lib.models"]}
+
+        try:
+            await tortoise.Tortoise.init(db_url=str(url), modules=modules)
+            _LOGGER.info("requiem has connected to the postgres server at <%s>!", url)
+
+        except (
+                tortoise.exceptions.DBConnectionError,
+                asyncpg.InvalidPasswordError,
+                ConnectionRefusedError,
+                socket.gaierror,
+        ):
+            await tortoise.Tortoise.init(db_url="sqlite://db.sqlite3", modules=modules)
+            _LOGGER.warning(
+                "requiem was unable to connect to a postgres server! sqlite will be used instead!"
+            )
+
+        await tortoise.Tortoise.generate_schemas()
+
+    async def load_extensions_on_start(self, event: hikari.StartingEvent) -> None:
         """
         Loads extensions prior to Requiem connecting to discord.
         Logs number of extensions loaded and number of commands gathered.
         """
+
         for extension in self.all_extensions:
             try:
                 self.load_extension(f"extensions.{extension}")
@@ -75,7 +115,7 @@ class Requiem(lightbulb.Bot, abc.ABC):
             f"successfully loaded {len(self.extensions)} extension(s) and {len(self.slash_commands)} command(s)!"
         )
 
-    async def on_stopping_event(self, event: hikari.StoppingEvent) -> None:
+    async def clean_up_on_closing(self, event: hikari.StoppingEvent) -> None:
         """
         Handles extension unloading and cleanup while Requiem is closing.
         """
@@ -91,4 +131,15 @@ class Requiem(lightbulb.Bot, abc.ABC):
                     exc_info=exc,
                 )
 
+        _LOGGER.info("successfully unloaded all extensions and commands!")
+
+        await tortoise.Tortoise.close_connections()
+
         _LOGGER.info("requiem has finished cleanup!")
+
+    async def log_successful_command_invoke(self, event: lightbulb.SlashCommandCompletionEvent) -> None:
+        """
+        Adds successful command execution logging to console.
+        """
+        _LOGGER.info(f"successfully executed command {event.command.name} in guild {event.context.guild_id}!")
+

3eb65681bd96c4069809f8a32bf694bb65963f03
Kyle Tyo <akikai297@gmail.com>
2021-12-23 13:07:17 -0500

    added reload command, logging, and type hinting

--- requiem/extensions/management/commands.py
+++ requiem/extensions/management/commands.py
@@ -17,11 +17,44 @@
 
 import lightbulb
 import hikari
+import logging
+
+
+_LOGGER = logging.getLogger("requiem.extensions.management")
 
 
 @lightbulb.command("terminate", "Terminate the bot.")
 @lightbulb.implements(lightbulb.SlashCommand)
-async def terminate(ctx: lightbulb.Context):
+async def terminate(ctx: lightbulb.Context) -> None:
     embed = hikari.Embed(description="Requiem is shutting down!")
     await ctx.respond(embed=embed)
     await ctx.bot.close()
+
+
+@lightbulb.option("extension", "The extension to be reloaded.")
+@lightbulb.command("reload", "Attempt to reload an extension.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def reload(ctx: lightbulb.Context) -> None:
+    extension = ctx.options.extension
+    embed = hikari.Embed()
+
+    try:
+        ctx.bot.reload_extensions(f"extensions.{extension}")
+        embed.description = f"The extension {extension} has been reloaded!!"
+
+    except lightbulb.ExtensionNotLoaded:
+        embed.description = f"The extension {extension} could not be reloaded because it is not loaded!"
+
+    except lightbulb.ExtensionNotFound:
+        embed.description = f"The extension {extension} could not be reloaded because it could not be found! If it " \
+                            f"already loaded, it has not been changed!"
+
+    except Exception as exc:
+        embed.description = f"The extension {extension} could not be reloaded because an unexpected exception was " \
+                            f"encountered! If it was already loaded, it has not been changed!"
+        _LOGGER.error(
+            f"encountered an exception while attempting to load {extension}!",
+            exc_info=exc,
+        )
+
+    await ctx.respond(embed=embed)

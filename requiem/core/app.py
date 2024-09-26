7864fff64104446a8f70a027ba2e648ede8bff52
Kyle Tyo <akikai297@gmail.com>
2024-09-25 22:02:09 -0400

    remove extra typing import

--- requiem/core/app.py
+++ requiem/core/app.py
@@ -1,6 +1,5 @@
 # This is part of Requiem
 # Copyright (C) 2020  Verin Senpai
-import typing
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

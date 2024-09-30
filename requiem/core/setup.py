53a9a9c23ed3a6ab71dba0250755a1cf66b55eaf
Kyle Tyo <akikai297@gmail.com>
2024-09-29 21:02:52 -0400

    setup wrap for discord token

--- requiem/core/setup.py
+++ requiem/core/setup.py
@@ -19,15 +19,23 @@
 from requiem.core.config import RequiemConfig
 from hikari import urls
 from datetime import datetime, timedelta
+from pathlib import Path
 
+import os
 import click
-import hikari
 import asyncio
 import aiohttp
 import logging
+import yaml
 
 
 _LOGGER = logging.getLogger("requiem.setup")
+
+
+def prompt_close():
+    click.pause("press any key to exit the script...")
+
+    exit(0)
 
 
 class RequiemSetup:
@@ -44,29 +52,62 @@ class RequiemSetup:
         return datetime.now() - self._start_time
 
     async def get_token(self) -> None:
-        while self.fail_count < 5:
-            current_token: str | None = self.current.discord_token
+        current: str | None = self.config.get("token")
 
-            self.updated["token"] = token = click.prompt(
-                "enter a discord token",
-                default=current_token,
-                show_default=bool(current_token)
+        while self._fail_count < 5:
+            self.config["token"] = input_token = click.prompt(
+                "discord token", default=current, show_default=bool(current)
             )
-
-            _LOGGER.info("setup is validating the provided token with discord.")
 
             async with aiohttp.ClientSession() as session:
                 response = await session.get(
                     f"{urls.REST_API_URL}/users/@me",
-                    headers={"Authorization": f"Bot {token}"}
+                    headers={"Authorization": f"Bot {input_token}"}
                 )
 
             if response.status == 200:
-                _LOGGER.info("token passed validation. proceeding with setup.")
-
                 return
 
-            _LOGGER.info("the token provided could not be validated! check your input and try again!")
+            click.echo("token could not be validated! check your input and try again!")
 
-    async def run(self) -> None:
-        await self.get_token()
+            await asyncio.sleep(5)
+
+        click.echo("failed to validate token within 5 attempts! setup is exiting!")
+
+        prompt_close()
+
+    async def setup_db(self) -> None:
+        config: dict = self.config.get("database")
+
+        while self._fail_count < 5:
+            config["host"] = click.prompt("database host", default=config["host"], show_default=True)
+            config["port"] = click.prompt("database port", default=config["port"], show_default=True)
+            config["user"] = click.prompt("database user", default=config["user"], show_default=True)
+            current: str | None = config["password"]
+            config["password"] = click.prompt("database password", default=current, show_default=bool(current))
+
+    async def run(self, instance_path: Path) -> None:
+        config_file: Path = instance_path / "config.yaml"
+
+        with config_file.open("w") as file:
+            await self.get_token()
+            self._fail_count = 0
+            await self.setup_db()
+
+            yaml.safe_dump(self.config, file)
+
+        _LOGGER.info("setup is complete! config saved to '%s'!", config_file)
+
+        confirm = click.confirm("would you like to create a run file?")
+
+        if not confirm:
+            return
+
+        py_call = "python" if os.name == "nt" else "python3"
+        ext = "bat" if os.name == "nt" else "sh"
+        script: str = f"{py_call} -OO -m requiem start"
+
+        with open(f"run_requiem.{ext}", "w") as file:
+            file.write(script)
+
+        _LOGGER.info("setup script created!")

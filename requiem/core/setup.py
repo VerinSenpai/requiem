f069a59236a7c7af41690c29593757329c7b6c2d
Kyle Tyo <akikai297@gmail.com>
2024-10-01 22:02:37 -0400

    "initial" impl of requiem setup
    needs more work but functions enough to get requiem going as is.
    
    TODO:
    impl setup for owner_ids
    impl more pre-setup guidance
    deal with file permission issues

--- requiem/core/setup.py
+++ requiem/core/setup.py
@@ -15,9 +15,8 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-
-from requiem.core.config import RequiemConfig, PostgresConfig
-from requiem.core.database import start_database
+from requiem.core.config import RequiemConfig, PostgresConfig, save_config
+from requiem.core.database import start_database, stop_database
 from requiem import __discord__
 from hikari import urls
 from datetime import datetime, timedelta
@@ -30,7 +29,7 @@ import aiohttp
 import logging
 import yaml
 import asyncpg
-import contextlib
+import os
 
 
 _LOGGER = logging.getLogger("requiem.setup")
@@ -41,21 +40,33 @@ class RequiemSetup:
     def __init__(self, instance_path: Path, config: RequiemConfig) -> None:
         self._start_time: datetime = datetime.now()
         self._fail_count: int = 0
+
         self.instance_path: Path = instance_path
-        config: RequiemConfig = config or global_converter.structure({}, RequiemConfig)
-        self.config = config.__dict__
-        self.config["database"] = config.database.__dict__
+        self.config: RequiemConfig = config or global_converter.structure({}, RequiemConfig)
 
     @property
     def session_time(self) -> timedelta:
         return datetime.now() - self._start_time
 
-    async def get_token(self) -> None:
-        current: str | None = self.config.get("token")
+    async def handle_fail(self, message: str):
+        click.echo(f"{message} check your input and try again!")
 
-        while self._fail_count < 5:
-            self.config["token"] = input_token = click.prompt(
-                "discord token", default=current, show_default=bool(current)
+        if self._fail_count >= 2:
+            click.echo(f"having trouble? feel free to ask for help in the requiem support discord! {__discord__}")
+
+        else:
+            self._fail_count += 1
+
+        await asyncio.sleep(5)
+
+    async def get_token(self) -> None:
+        current: str | None = self.config.token
+
+        while True:
+            self.config.token = input_token = click.prompt(
+                "discord token",
+                default=current,
+                show_default=bool(current)
             )
 
             async with aiohttp.ClientSession() as session:
@@ -67,61 +78,36 @@ class RequiemSetup:
             if response.status == 200:
                 return
 
-            elif self._fail_count < 4:
-                click.echo("token could not be validated! check your input and try again!")
-                await asyncio.sleep(5)
-
-            self._fail_count += 1
-
-        click.echo("failed to validate token within 5 attempts!")
+            await self.handle_fail("validation of discord token failed!")
 
     async def setup_database(self) -> None:
-        config: dict = self.config.get("database")
+        config: PostgresConfig = self.config.database
 
-        while self._fail_count < 5:
-            config["host"] = click.prompt("database host", default=config["host"], show_default=True)
-            config["port"] = click.prompt("database port", default=config["port"], show_default=True)
-            config["user"] = click.prompt("database user", default=config["user"], show_default=True)
-            config["password"] = click.prompt(
-                "database password", default=config["password"], show_default=bool(config["password"])
+        while True:
+            config.host = click.prompt("host", default=config.host, show_default=True)
+            config.port = click.prompt("port", default=config.port, show_default=True, type=int)
+            config.user = click.prompt("user", default=config.user, show_default=True)
+            config.password = click.prompt(
+                "password",
+                default=config.password,
+                show_default=bool(config.password)
             )
-            config["path"] = f"/{self.instance_path.name}"
+            config.path = f"/{self.instance_path.name}"
 
-            test_config: PostgresConfig = global_converter.structure(config, PostgresConfig)
-
-            with contextlib.suppress(asyncpg.InvalidAuthorizationSpecificationError):
-                await start_database(self.instance_path, test_config)
-                self.config["database"] = config
+            try:
+                await start_database(self.instance_path, config)
+                self.config.database = config
+                await stop_database()
                 return
 
-            if self._fail_count < 4:
-                click.echo("database config could not be validated! check your inputs and try again!")
-                await asyncio.sleep(5)
-
-            self._fail_count += 1
-
-        click.echo("failed to validate database credentials within 5 attempts!")
+            except asyncpg.InvalidAuthorizationSpecificationError:
+                await self.handle_fail("validation of database config failed!")
 
     async def run(self) -> None:
-        config_file: Path = self.instance_path / "config.yaml"
+        await self.get_token()
+        await self.setup_database()
 
-        process = (
-            self.get_token,
-            self.setup_database
-        )
+        save_config(self.instance_path, self.config)
 
-        with config_file.open("w") as file:
-            for job in process:
-                self._fail_count = 0
-                await job()
-
-                if self._fail_count == 5:
-                    click.echo(f"you appear to be having difficulty with setup! if you need assistance,\n"
-                               f"feel free to ask for help in the requiem support server!\n\n{__discord__}")
-
-                    _LOGGER.warning("setup aborted! no changes have been saved!")
-                    return
-
-            yaml.safe_dump(self.config, file)
-
-        _LOGGER.info("setup is complete! config saved to '%s'!", config_file)
+        python = "python" if os.name == "nt" else "python3"
+        _LOGGER.info(f"run '{python} -OO -m requiem start' in your terminal to start requiem!")

b9692ed950e7c034aca2b57b35b568d5912f1921
Kyle Tyo <akikai297@gmail.com>
2024-10-05 23:20:15 -0400

    added GuildsConfig and UsersConfig database models

--- requiem/core/models.py
+++ requiem/core/models.py
@@ -13,3 +13,14 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from tortoise import Model, fields
+
+
+class GuildsConfig(Model):
+    guild_id: int = fields.BigIntField(primary_key=True)
+
+
+class UsersConfig(Model):
+    user_id: int = fields.BigIntField(primary_key=True)

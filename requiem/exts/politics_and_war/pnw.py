527a420753b384225353c34d4cb0012559fe8326
Kyle Tyo <akikai297@gmail.com>
2024-10-20 18:33:03 -0400

    progress commit

--- requiem/exts/politics_and_war/pnw.py
+++ requiem/exts/politics_and_war/pnw.py
@@ -20,6 +20,7 @@ from requiem.core.config import PnWConfig
 
 import lightbulb
 import pwpy
+import hikari
 
 
 plugin = lightbulb.Plugin("PW")
@@ -34,20 +35,83 @@ async def pw(ctx: RequiemContext) -> None:
 
 
 @pw.child
+@lightbulb.add_cooldown(10, 1, lightbulb.UserBucket)
 @lightbulb.command("nation", "View information for a specified nation.")
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def nation(ctx: RequiemContext) -> None:
     query = {
         "model": "nations",
         "args": {"id": 34904},
-        "fields": {
+        "query": {
             "data": (
+                {"alliance": ("name", "id")},
+                {"cities": ("infrastructure", "land")},
                 "nation_name",
-                {"alliance": ("name", "id")}
-            )
+                "leader_name",
+                "id",
+                "num_cities",
+                "population",
+                "domestic_policy",
+                "war_policy",
+                "soldiers",
+                "tanks",
+                "aircraft",
+                "ships",
+                "missiles",
+                "nukes",
+                "flag",
+                "score",
+                "date",
+                "color"
+            ),
         }
     }
 
-    resp = await pwpy.api.get_query(query, pw_config.api_key)
+    response = await pwpy.api.get_query(query, pw_config.api_key)
+    _nation = pwpy.models.Nation(response["nations"]["data"][0])
 
-    await ctx.respond(resp)
+    infra = land = 0
+    for city in _nation.cities:
+        infra += city.infra
+        land += city.land
+
+    nation_url: str = f"{pwpy.urls.NATION_PAGE}/id={_nation.id}"
+    message_url: str = f"{pwpy.urls.MESSAGE_PAGE}/receiver={_nation.leader_name}".replace(" ", "%20")
+    embed = hikari.Embed(
+        description=f"[{_nation.nation_name}]({nation_url}) - [{_nation.leader_name}]({message_url})",
+        color=ctx.color
+    )
+
+    if _nation.alliance is not None:
+        alliance_url = f"{pwpy.urls.ALLIANCE_PAGE}/id={_nation.alliance.id}"
+        embed.add_field(name="Alliance", value=f"[{_nation.alliance.name}]({alliance_url})")
+
+    score = _nation.score
+    embed.add_field(name="Score", value=str(score), inline=True)
+    min_range, max_range = pwpy.utils.score_range(score)
+    range_url = (f"{pwpy.urls.WARS_PAGE}&keyword={score}&cat=war_range"
+                 f"&ob=score&od=ASC&beige=true&vmode=false&openslots=true")
+    embed.add_field(
+        name="Strike Range",
+        value=f"[{round(min_range, 2)} - {round(max_range, 2)}]({range_url})",
+        inline=True
+    )
+
+    cities_url = f"{pwpy.urls.CITY_MANAGER_PAGE}&l={_nation}"
+    embed.add_field(name="Cities", value=f"[{_nation.num_cities}]({cities_url})", inline=True)
+    embed.add_field(name="Population", value=f'{_nation.population:,}', inline=True)
+    embed.add_field(name="Infra", value=str(infra), inline=True)
+    embed.add_field(name="Land", value=str(land), inline=True)
+    embed.add_field(name="Color", value=_nation.color, inline=True)
+    embed.add_field(name="Domestic Policy", value=_nation.domestic_policy, inline=True)
+    embed.add_field(name="War Policy", value=_nation.war_policy, inline=True)
+    embed.add_field(name="Soldiers", value=f'{_nation.soldiers:,}', inline=True)
+    embed.add_field(name="Tanks", value=f'{_nation.tanks:,}', inline=True)
+    embed.add_field(name="Aircraft", value=f'{_nation.aircraft:,}', inline=True)
+    embed.add_field(name="Ships", value=f'{_nation.ships:,}', inline=True)
+    embed.add_field(name="Missiles", value=f'{_nation.missiles:,}', inline=True)
+    embed.add_field(name="Nukes", value=f'{_nation.nukes:,}', inline=True)
+    embed.set_image(_nation.flag)
+
+    await ctx.respond(embed=embed)
+
